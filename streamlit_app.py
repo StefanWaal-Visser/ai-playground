d4669564ebf34d9102d081236239a49454b41f33
StefanWaal-Visser <srvanderwaal@visser-visser.nl>
2024-09-27 09:25:11 +0000

    Startpagina

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Welkom bij de Visser & Visser AI Playground")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+"""In deze omgeving zijn verschillende AI modules beschikbaar die door het team IT, Data & Innovatie is ontwikkeld. De modules worden allemaal in Bèta versie vrijgegeven. 
+Houdt rekening met instabiliteit en onverwachte uitkomsten.
+Wij nodigen iedere gebruiker van harte uit alle opbouwende feedback te delen.
+Mail alle opmerkingen, vragen en complimenten naar diu@visser-visser.nl.""")
